c6947435d60cbadaea2c34a20c397aaf0051c942
senyonyo <t16438as@sfc.keio.ac.jp>
2017-03-03 12:20:49 +0000

    try

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE>
+<html lang="ja">
+<head>
+	<meta charset="utf8">
+	<title>簡易掲示板</title>
+</head>
+<body>
+	<h1>簡易掲示板</h1>
+	<form>
+		message:<input type="text" name="message">
+		user:<input tyw="text" name="user">
+		<input type="submit value="投稿"">	
+	</form>
+	<h2>投稿一覧(0件)</h2>
+	<ul>
+		<li>まだ投稿はありません。</li>
+	</ul>
+</body>
+</html>
